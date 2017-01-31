ccc46e1c8c249a52860d12a3e20bd096ee901987
alexandr <emaxprog@gmail.com>
2017-01-31 13:20:43 +0300

    First Commit

---
+++ Plugin.php
@@ -0,0 +1,94 @@
+<?php namespace Em\YMap;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * YMap Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'YMap',
+            'description' => 'No description provided yet...',
+            'author'      => 'Em',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return [
+            'Em\YMap\Components\Map' => 'map',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'em.ymap.some_permission' => [
+                'tab' => 'YMap',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'ymap' => [
+                'label'       => 'YMap',
+                'url'         => Backend::url('em/ymap/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['em.ymap.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
---
+++ components/Map.php
@@ -0,0 +1,66 @@
+<?php namespace Em\Ymap\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Map extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Map Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'latitude' => [
+                'title' => 'Широта',
+                'default' => 51.506767,
+                'type' => 'string',
+            ],
+            'longitude' => [
+                'title' => 'Долгота',
+                'default' => -0.122205,
+                'type' => 'string',
+            ],
+            'zoom' => [
+                'title' => 'Масштаб',
+                'default' => 12,
+                'type' => 'string',
+            ],
+            'width' => [
+                'title' => 'Ширина',
+                'default' => '100%',
+                'description' => 'Может быть задана в px или %',
+                'type' => 'string',
+            ],
+            'height' => [
+                'title' => 'Высота',
+                'default' => '350px',
+                'description' => 'Может быть задана в px или %',
+                'type' => 'string',
+            ],
+            'mapTypeId' => [
+                'title'             => 'Map Type',
+                'default'           => 'yandex#map',
+                'type'              => 'dropdown',
+                'options'           => ['yandex#map'=>'Roadmap', 'yandex#satellite'=>'Satellite', 'yandex#hybrid'=>'Hybrid']
+
+            ],
+            'hintContent' => [
+                'title' => 'Хинт',
+                'default' => 'Москва!',
+                'description' => 'Подсказка на карте',
+                'type' => 'string',
+            ],
+            'balloonContent' => [
+                'title' => 'Высота',
+                'default' => 'Столица России!',
+                'description' => 'Всплывающее окно на карте',
+                'type' => 'string',
+            ]
+        ];
+    }
+}
